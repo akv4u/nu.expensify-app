6c29fd815f9eb49e993761197fa941abaa54927c
akv4u <akv4u@example.com>
2018-06-22 16:22:34 +0100

    Redux Play

--- src/components/EditExpensePage.js
+++ src/components/EditExpensePage.js
@@ -1,9 +1,11 @@
 import React from 'react'
 
-const EditExpensePage = () => (
+const EditExpensePage = (props) => {
+  console.log (props)
+  return (
   <div>
-    This is my Edit expense component
+    Editing the expense id : {props.match.params.id}
   </div>
-);
+)};
 
 export default EditExpensePage
